e358c6c93434a374a8d4675cfd1e50e49f46db59
abdourifki <abdourifki1995@gmail.com>
2024-01-19 11:48:45 +0100

    add middleware

---
+++ middleware/authMiddleware.js
@@ -0,0 +1,28 @@
+const jwt = require('jsonwebtoken');
+const dotenv = require("dotenv");
+dotenv.config();
+
+
+
+const requireAuth = (req, res, next) => {
+    const token = req.cookies.jwt
+
+ // check json web token exist and verified
+  if(token){
+    jwt.verify(token,process.env.JWT_SECRET_KEY, (err, decodedToken)=>{
+      if(err){
+        next();
+        
+      }else{
+        console.log(decodedToken);
+        next();
+      }
+      
+    })
+  }
+  else{
+    res.status(401).json({message:'not token'});
+  }
+}
+
+module.exports = requireAuth;
